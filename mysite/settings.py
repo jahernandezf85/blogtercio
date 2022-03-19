188d5d8bf557e73d3992995ce897491bdb01fc5d
Jose Alberto <jahernandezf@gmail.com>
2022-03-19 04:29:26 +0100

    Subida cambiando de nuevo una vez recogidos los staticos"

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #local
-#STATICFILES_DIRS = [ 
-#    os.path.join(BASE_DIR, 'static')
-#    ]
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static')
+    ]
 
 #server
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 
 
