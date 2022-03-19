2d4b9e8b4a4f6ef900fa1e2f2cbc649a03f6611b
Jose Alberto <jahernandezf@gmail.com>
2022-03-19 04:27:07 +0100

    Se modifica para que funcione en el servidor

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
-    ]
+
+#local
+#STATICFILES_DIRS = [ 
+#    os.path.join(BASE_DIR, 'static')
+#    ]
+
+#server
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 
 
